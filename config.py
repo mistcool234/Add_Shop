75e1a501bbcac6e161e7ef5d109614cf2901602e
Destinymartins08 <destiny.martins@binghamuni.edu.ng>
2025-07-24 21:13:37 +0100

    Add files via upload

---
+++ config.py
@@ -0,0 +1,9 @@
+# config.py
+
+DB_CONFIG = {
+    'host': 'localhost',
+    'user': 'root',          # default XAMPP username
+    'password': '',          # default password is blank (unless you changed it)
+    'database': 'tv_store',  # the name of your MySQL database
+    'port': 3306             # default MySQL port in XAMPP (unless changed to 3307)
+}
